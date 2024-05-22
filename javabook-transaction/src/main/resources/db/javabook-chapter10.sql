500a857ab1c66e6cbdf0f0a00f130be58d56a5ec
湘王 <125043411@qq.com>
2024-05-22 21:51:36 +0800

    modified sql

--- javabook-transaction/src/main/resources/db/javabook-chapter10.sql
+++ javabook-transaction/src/main/resources/db/javabook-chapter10.sql
@@ -1,6 +1,8 @@
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
+CREATE DATABASE IF NOT EXISTS `javabook` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 -- ----------------------------
 -- Table structure for sys_accounting_voucher
 -- ----------------------------
